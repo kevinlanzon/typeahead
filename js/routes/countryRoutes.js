01a76218824fbaefc214e587e320fb5b1008bf47
Kevin Lanzon <kevinlanzon@gmail.com>
2017-12-31 19:16:14 +0000

    Adds ui-router and moves directive out of index.html

---
+++ js/routes/countryRoutes.js
@@ -0,0 +1,14 @@
+app.config(function($stateProvider) {
+
+$stateProvider
+
+  .state('search', {
+    url: '',
+    views: {
+      content: {
+      templateUrl: 'views/search.html'
+      }
+    }
+  })
+  
+});
